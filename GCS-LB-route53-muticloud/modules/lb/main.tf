75d9a799e1df1d4c18a38e7ddf2deb6ba10af358
shubham <shubham.shastri1994@gmail.com>
2024-05-30 12:09:27 +0530

    added multicloud tf

--- GCS-LB-route53-muticloud/modules/lb/main.tf
+++ GCS-LB-route53-muticloud/modules/lb/main.tf
@@ -1,9 +1,9 @@
-resource "google_compute_backend_bucket" "default" {
+resource "google_compute_backend_bucket" "bucket" {
   name       = var.bucket_name
   bucket_name = var.bucket_name
 }
 
-resource "google_compute_managed_ssl_certificate" "default" {
+resource "google_compute_managed_ssl_certificate" "cert" {
   name = var.ssl_certificate_name
 
   managed {
@@ -11,20 +11,20 @@ resource "google_compute_managed_ssl_certificate" "default" {
   }
 }
 
-resource "google_compute_url_map" "default" {
+resource "google_compute_url_map" "mapping" {
   name            = var.url_map_name
   default_service = google_compute_backend_bucket.default.self_link
 }
 
-resource "google_compute_target_https_proxy" "default" {
+resource "google_compute_target_https_proxy" "proxy" {
   name             = var.target_proxy_name
   url_map          = google_compute_url_map.default.self_link
-  ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+  ssl_certificates = [google_compute_managed_ssl_certificate.cert.id]
 }
 
-resource "google_compute_global_forwarding_rule" "default" {
+resource "google_compute_global_forwarding_rule" "rule" {
   name       = var.forwarding_rule_name
-  target     = google_compute_target_https_proxy.default.self_link
+  target     = google_compute_target_https_proxy.proxy.self_link
   port_range = "443"
 }
 
